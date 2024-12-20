7b5e7a188c1ffe92f339a7b33f312d9e74a7b42d
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2024-12-20 13:32:23 +0530

    Create 20-12-2024.cpp

---
+++ LEETCODE_POTD/20-12-2024.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        int level=0;
+        vector<TreeNode*>v;
+
+        while(!q.empty())
+        {
+            int n=q.size();
+            while(n--)
+            {
+                TreeNode* front=q.front();
+                q.pop();
+                if(level%2)
+                v.push_back(front);
+                if(front->left)     q.push(front->left);
+                if(front->right)    q.push(front->right);
+            }
+            if(level%2)
+            {
+                int l=0,r=v.size()-1;
+                while(l<r)
+                {
+                    swap(v[l]->val,v[r]->val);
+                    l++;
+                    r--;
+                }
+            }
+            v.clear();
+            level++;
+        }
+        return root;
+    }
+};
