4c37a54a896d78022a0a5e09a78dabfde617645a
ravi99 <menariyaravi6@gmail.com>
2025-01-28 13:38:50 +0530

    from vs to check

--- LEETCODE_POTD/Jan 2025/01-01-2025.cpp
+++ LEETCODE_POTD/Jan 2025/01-01-2025.cpp
@@ -1,20 +1,22 @@
-class Solution {
+class Solution
+{
 public:
-  int maxScore(string s) {
-        int left = s[0]=='0'?1:0;
-        int right=0;
-        for(int i=1;i<s.length();i++){
-            right+=(s[i]=='1')?1:0;
+    int maxScore(string s)
+    {
+        int left = s[0] == '0' ? 1 : 0;
+        int right = 0;
+        for (int i = 1; i < s.length(); i++)
+        {
+            right += (s[i] == '1') ? 1 : 0;
         }
         int maxsum = left + right;
-        for(int j =1 ;j<s.length()-1;j++){
-            if(s[j]=='0'){
-                left+=1;
-            }
-            else{
-                    right-=1;
-            }
-            maxsum = max(maxsum , left+right);
+        for (int j = 1; j < s.length() - 1; j++)
+        {
+            if (s[j] == '0')
+                left += 1;
+            else
+                right -= 1;
+            maxsum = max(maxsum, left + right);
         }
         return maxsum;
     }
