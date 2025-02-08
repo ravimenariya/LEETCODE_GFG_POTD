55bf186083074519823e01ecc4e9e49024d167f1
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-02-09 00:23:24 +0530

    Create 08-02-2025.cpp

---
+++ GFG_POTD/Feb 2025/08-02-2025.cpp
@@ -0,0 +1,53 @@
+
+// problem -> https://www.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
+
+class Solution {
+  public:
+    void left(Node *root,vector<int>&ans){
+        if(!root) return;
+        if(root->left){
+            ans.push_back(root->data);
+            left(root->left,ans);
+        }
+        else if(root->right){
+            ans.push_back(root->data);
+            left(root->right,ans);
+        }
+    }
+    
+    void bottom(Node *root,vector<int>&ans){
+        if(!root) return ;
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(root->data);
+            return;
+            
+        }
+        if(root->left) bottom (root->left,ans);
+        if(root->right) bottom (root->right,ans);
+    }
+    void right(Node *root,vector<int>&ans){
+        if(!root) return;
+        if(root->right){
+            right(root->right,ans);
+            ans.push_back(root->data);
+            
+        }
+        else if(root->left){
+            
+            right(root->left,ans);
+            ans.push_back(root->data);
+        }
+    }
+    vector<int> boundaryTraversal(Node *root) {
+        //CodeGenius
+        
+        vector<int>ans;
+        if(!root) return ans;
+        ans.push_back(root->data);
+        left(root->left,ans);
+        bottom(root->left,ans);
+        bottom(root->right,ans);
+        right(root->right,ans);
+        return ans;
+    }
+};
