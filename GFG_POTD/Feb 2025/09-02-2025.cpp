b0c44af6f045eca91fd78e9080942dc977ae7439
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-02-09 22:17:20 +0530

    Create 09-02-2025.cpp

---
+++ GFG_POTD/Feb 2025/09-02-2025.cpp
@@ -0,0 +1,22 @@
+
+// Problem -> https://www.geeksforgeeks.org/problems/maximum-path-sum-from-any-node/1
+
+class Solution {
+    int solve(Node* root,int&ans)
+    {
+        if(!root)   return 0;
+        int left=solve(root->left,ans);
+        int right=solve(root->right,ans);
+        int m1=max({root->data,root->data+left,root->data+right});
+        ans=max({ans,m1,root->data+left+right});
+        return m1;
+    }
+  public:
+    // Function to return maximum path sum from any node in a tree.
+    int findMaxSum(Node *root) {
+        // code here
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
