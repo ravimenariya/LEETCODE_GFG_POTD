fadd0f423efb13379474ebed1b6cb31fc9a73648
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-02-23 23:46:26 +0530

    Update 23-02-2025.cpp

--- LEETCODE_POTD/Feb 2025/23-02-2025.cpp
+++ LEETCODE_POTD/Feb 2025/23-02-2025.cpp
@@ -1,21 +1,23 @@
 
-// problem -> https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+// problem -> https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/submissions/1553059043/?envType=daily-question&envId=2025-02-23
 
 class Solution {
-  public:
-    // Function to find the next greater element for each element of the array.
-    vector<int> nextLargerElement(vector<int>& arr) {
-        // code here
-        int n=arr.size();
-        vector<int>ans(n);
-        stack<int>stk;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!stk.empty() && stk.top()<=arr[i])    stk.pop();
-            if(stk.empty()) ans[i]=-1;
-            else    ans[i]=stk.top();
-            stk.push(arr[i]);
-        }
-        return ans;
+    TreeNode* solve(vector<int>& preorder, vector<int>& postorder,int pre_st,int pre_end,int post_st,int post_end,unordered_map<int,int>&post_idx)
+    {
+        if(pre_st>pre_end)  return NULL;
+        TreeNode* root=new TreeNode(preorder[pre_st]);
+        if(pre_st+1>pre_end) return root;
+        int nend=post_idx[preorder[pre_st+1]];
+        int size=nend-post_st;
+        root->left=solve(preorder,postorder,pre_st+1,pre_st+1+size,post_st,nend,post_idx);
+        root->right=solve(preorder,postorder,pre_st+1+size+1,pre_end,nend+1,post_end-1,post_idx);
+        return root;
+    }
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        int n=preorder.size();
+        unordered_map<int,int>post_idx;
+        for(int i=0;i<n;i++)    post_idx[postorder[i]]=i;
+        return solve(preorder,postorder,0,n-1,0,n-1,post_idx);
     }
 };
