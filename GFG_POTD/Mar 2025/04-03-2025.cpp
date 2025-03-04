b326a6c55d73cb25c32139e18f732be89ef159f6
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-03-04 21:34:26 +0530

    Update 04-03-2025.cpp

--- GFG_POTD/Mar 2025/04-03-2025.cpp
+++ GFG_POTD/Mar 2025/04-03-2025.cpp
@@ -7,16 +7,9 @@ class Solution {
         vector<int>ans;
         ans.push_back(arr[0]);
         for(int i=1;i<arr.size();i++)
-        {
             if(ans.back()<arr[i])   ans.push_back(arr[i]);
-            else
-                for(int j=0;j<ans.size();j++)
-                    if(ans[j]>=arr[i])
-                    {
-                        ans[j]=arr[i];
-                        break;
-                    }
-        }
+            else    *lower_bound(ans.begin(),ans.end(),arr[i])=arr[i];
+
         return ans.size();
     }
 };
