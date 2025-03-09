900ac2e7beeaf50f564d73b09e2620dfe139d0ec
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-03-09 23:21:40 +0530

    Create 09-03-2025.cpp

---
+++ GFG_POTD/Mar 2025/09-03-2025.cpp
@@ -0,0 +1,36 @@
+
+// Problem -> https://www.geeksforgeeks.org/problems/count-palindrome-sub-strings-of-a-string0652/1
+
+class Solution {
+  public:
+    int countPS(string &S) {
+        // code here
+        int c=0,n=S.size();
+        int left,right;
+        
+        for(int i=0;i<n;i++)
+        {
+            left=i-1;
+            right=i;
+            
+            while(left>=0 && right<n && S[left]==S[right])
+            {
+                c++;
+                left--;
+                right++;
+            }
+            
+            left=i-1;
+            right=i+1;
+            
+            while(left>=0 && right<n && S[left]==S[right])
+            {
+                c++;
+                left--;
+                right++;
+            }
+        }
+        
+        return c;
+    }
+};
