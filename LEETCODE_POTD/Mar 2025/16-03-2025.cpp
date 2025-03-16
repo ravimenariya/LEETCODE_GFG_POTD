db36d7b0314b5014ed96c4829bfdf631f393884f
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-03-16 11:23:24 +0530

    Create 16-03-2025.cpp

---
+++ LEETCODE_POTD/Mar 2025/16-03-2025.cpp
@@ -0,0 +1,29 @@
+
+// Proble -> https://leetcode.com/problems/minimum-time-to-repair-cars/?envType=daily-question&envId=2025-03-16
+
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long left = 1, right = (long long)*min_element(ranks.begin(), ranks.end()) * cars * cars;
+        
+        auto can_repair_all = [&](long long time) {
+            long long total_cars_repaired = 0;
+            for (int rank : ranks) {
+                total_cars_repaired += sqrt(time / rank);
+                if (total_cars_repaired >= cars) return true;
+            }
+            return false;
+        };
+        
+        while (left < right) {
+            long long mid = (left + right) / 2;
+            if (can_repair_all(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
