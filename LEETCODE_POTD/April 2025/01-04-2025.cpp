e368d5a2858ca1372fbc23b1bfc80a8345acdc91
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-04-02 11:03:18 +0530

    Create 01-04-2025.cpp

---
+++ LEETCODE_POTD/April 2025/01-04-2025.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n=nums.size();
+        long long ans=0,temp;
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
+                for(int k=j+1;k<n;k++)
+                {
+                    temp=((long long)(nums[i]-nums[j]))*nums[k];
+                    ans=max(ans,temp);
+                }
+        return ans;
+    }
+};
