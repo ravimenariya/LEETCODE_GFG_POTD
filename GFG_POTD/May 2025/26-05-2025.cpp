9c71355757a5bfaa0d79b9e23472b56248875a62
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-05-26 23:49:33 +0530

    Create 26-05-2025.cpp

---
+++ GFG_POTD/May 2025/26-05-2025.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int data) {
+        // code here
+        Node* n = new Node(data);
+        if (!head) {
+            n->next = n;
+            return n;
+        }
+        Node* cur = head;
+        while (true) {
+            Node* nxt = cur->next;
+            if ((cur->data <= data && data <= nxt->data) ||
+                (cur->data > nxt->data && (data >= cur->data || data <= nxt->data)) ||
+                nxt == head) {
+                cur->next = n;
+                n->next = nxt;
+                if (data < head->data) head = n;
+                break;
+            }
+            cur = nxt;
+        }
+        return head;
+    }
+};
