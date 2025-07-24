737391e9418624d12b19e0992b769a17532c6b19
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-07-24 17:01:13 +0530

    Create 24 Last Moment Before All Ants Fall Out.cpp

---
+++ GFG_POTD/July 2025/24 Last Moment Before All Ants Fall Out.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int ans=0;
+        for(int i:left)     ans=max(ans,i);
+        for(int i:right)    ans=max(ans,n-i);
+        return ans;
+    }
+};
