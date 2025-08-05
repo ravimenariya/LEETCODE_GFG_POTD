6137757db3ed3fce73fed7ec9fe51e3cb1bd01b6
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-08-05 10:18:16 +0530

    Create 5 Palindrome Sentence.cpp

---
+++ GFG_POTD/Aug 2025/5 Palindrome Sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isPalinSent(string &s) {
+        // code here
+        int i=0,j=s.size()-1;
+        while(i<j)
+        {
+            if(!isalnum(s[i]))         i++;
+            else if(!isalnum(s[j]))    j--;
+            else if(tolower(s[i])!=tolower(s[j]))         return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
