3d1240c64796a3dd30cdf9fd6064582e3e565d2f
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-08-20 11:55:59 +0530

    Create 20 Count Square Submatrices with All Ones.cpp

---
+++ LEETCODE_POTD/Aug 2025/20 Count Square Submatrices with All Ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size(),count=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j] && i && j)
+                    matrix[i][j]=min({matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]})+1;
+                count+=matrix[i][j];
+            }
+        return count;
+    }
+};
