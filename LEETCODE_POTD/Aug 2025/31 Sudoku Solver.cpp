252b1816e0ace02eff517a05a4b6a88e7d3adc93
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-08-31 22:48:31 +0530

    Create 31 Sudoku Solver.cpp

---
+++ LEETCODE_POTD/Aug 2025/31 Sudoku Solver.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char d){
+        for(int i=0; i<9; i++){
+            if(board[row][i] == d) return false;
+            if(board[i][col] == d) return false;
+        }
+
+        int start_i = (row/3) * 3;
+        int start_j = (col/3) * 3;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(board[start_i+i][start_j+j] == d) return false;
+            }
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.') continue;
+
+                for(char d='1'; d<='9'; d++){
+                    if(isValid(board, i, j, d)){
+                        board[i][j] = d;
+                        if(solve(board)) return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
