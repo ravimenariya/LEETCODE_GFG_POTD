c967d7f71e2c2027717d1b0d19cc2be55b142528
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-09-01 15:11:59 +0530

    Update 01 Maximum Average Pass Ratio.cpp

--- LEETCODE_POTD/Sept 2025/01 Maximum Average Pass Ratio.cpp
+++ LEETCODE_POTD/Sept 2025/01 Maximum Average Pass Ratio.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         priority_queue<pair<double,int>>pq;
-        for(int i=0;i<classes.size();i++)
-        {
+        for(int i=0;i<classes.size();i++){
             int p=classes[i][0],t=classes[i][1];
             double inc=(double)(p+1)/(t+1) - (double)p/t;
             pq.push({inc,i});
