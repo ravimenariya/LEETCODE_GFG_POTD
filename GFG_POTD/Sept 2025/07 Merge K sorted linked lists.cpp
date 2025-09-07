0a06067013a4269f52db7e36712d7a7f2d447730
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-09-07 21:06:07 +0530

    Create 07 Merge K sorted linked lists.cpp

---
+++ GFG_POTD/Sept 2025/07 Merge K sorted linked lists.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    Node* mergeKLists(vector<Node*>& arr) {
+        // Your code here
+        auto compare= [](Node* a,Node* b){ return a->data > b->data; };
+        priority_queue<Node*,vector<Node*>,decltype(compare)>pq(compare);
+        
+        for(auto n:arr)
+            pq.push(n);
+
+        Node* head=pq.top();
+        if(pq.top()->next)  pq.push(pq.top()->next);
+        pq.pop();
+        Node* temp=head;
+
+        while(!pq.empty())
+        {
+            temp->next=pq.top();
+            if(pq.top()->next)  pq.push(pq.top()->next);
+            pq.pop();
+            temp=temp->next;
+        }
+        return head;
+    }
+};
