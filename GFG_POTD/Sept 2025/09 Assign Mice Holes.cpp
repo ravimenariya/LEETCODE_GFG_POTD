33f242286f009c3a45a23f61e645e876e07cd1cf
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-09-10 14:54:18 +0530

    Create 09 Assign Mice Holes.cpp

---
+++ GFG_POTD/Sept 2025/09 Assign Mice Holes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int assignHole(vector<int>& mices, vector<int>& holes) {
+        // code here
+        sort(mices.begin(),mices.end());
+        sort(holes.begin(),holes.end());
+        
+        int ans=0;
+        for(int i=0;i<mices.size();i++)
+            ans=max(ans,abs(mices[i]-holes[i]));
+        return ans;
+    }
+};
