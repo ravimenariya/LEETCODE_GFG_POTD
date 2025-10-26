09b802dac09d41201ac6893fc535e655376a9e7a
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-10-26 22:38:27 +0530

    Implement minCost function for rope connection problem

---
+++ GFG_POTD/Oct 2025/26 Minimum Cost of ropes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        int count=0;
+        priority_queue<int,vector<int>, greater<int>> pq;
+        for(auto it:arr)
+        pq.push(it);
+        while(pq.size()!=1)
+        {
+            int first=pq.top();
+            pq.pop();
+            int second=pq.top();
+            pq.pop();
+            int total=first+second;
+            pq.push(total);
+            count+=total;
+        }
+        return count;
+    }
+
+};
