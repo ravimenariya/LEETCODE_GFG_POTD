02ac12b0ef472b68f967f7bc6da6e60052794c4e
Ravi Menariya <112074438+ravimenariya@users.noreply.github.com>
2025-12-19 18:41:51 +0530

    Create 19 Bus Conductor.cpp

---
+++ GFG_POTD/Dec 2025/19 Bus Conductor.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int findMoves(vector<int>& chairs, vector<int>& passengers) {
+        sort(chairs.begin(),chairs.end());
+        sort(passengers.begin(),passengers.end());
+        int ans=0;
+        for(int i=0;i<chairs.size();i++)
+            ans+=abs(chairs[i]-passengers[i]);
+        return ans;
+    }
+};
